56f1a819ca1245c3c429412f04ceefd74db62f62
Brainhell <ejrl64@hotmail.com>
2020-06-14 19:11:57 -0400

    Updates

---
+++ precache-manifest.826d6e2143631ef3caf8921f36db981e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d8556f340cbc04ffcf70b178df682f",
+    "url": "/bookworm-react/index.html"
+  },
+  {
+    "revision": "b047d5a92555f4c5e9cc",
+    "url": "/bookworm-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7210e52d0fede536136f",
+    "url": "/bookworm-react/static/js/2.cd1b6285.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bookworm-react/static/js/2.cd1b6285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b047d5a92555f4c5e9cc",
+    "url": "/bookworm-react/static/js/main.54af20b2.chunk.js"
+  },
+  {
+    "revision": "9055e80ba6567c13dc20",
+    "url": "/bookworm-react/static/js/runtime-main.3ede5d74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bookworm-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
